df1bdcc82a31690c339e39bad2d474cd99bc809f
Toby Jorris <tobyjorris@gmail.com>
2020-04-18 12:16:55 -0500

    added Print Recipe button, some misc styling

---
+++ src/app/recipe-modal/recipe-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-modal',
+  templateUrl: './recipe-modal.component.html',
+  styleUrls: ['./recipe-modal.component.css']
+})
+export class RecipeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

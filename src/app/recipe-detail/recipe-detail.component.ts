ff6d608c18492e9817fba45f2589865d25bcbb62
Toby Jorris <tobyjorris@gmail.com>
2020-04-17 20:10:24 -0500

    changed recipe title & instructions to be 'dynamic'

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
+
 export class RecipeDetailComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.selectedRecipe = new Recipe();
+    this.selectedRecipe.title = 'White Chicken Chili';
+    this.selectedRecipe.instructions = 'Cook until done, eat until full';
   }
 
 }
---
+++ src/recipe.ts
@@ -0,0 +1,4 @@
+export class Recipe {
+  title: string;
+  instructions: string;
+}

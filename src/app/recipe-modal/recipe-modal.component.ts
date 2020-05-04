9f8912e5dc43e540ce55c014b7fb33f887f6c4ce
Toby Jorris <tobyjorris@gmail.com>
2020-05-04 14:17:46 -0500

    Simplified recipe-card view, got the bootstrap grid system working on 'all recipes', updated the recipe detail modal to include more detail.

--- src/app/recipe-modal/recipe-modal.component.ts
+++ src/app/recipe-modal/recipe-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {Recipe} from '../../recipe';

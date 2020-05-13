8e68b71a6be7815cced08e05f6930b4279bff737
Toby Jorris <tobyjorris@gmail.com>
2020-05-13 10:03:21 -0500

    removed recipe-search-bar.component.html as it was no longer needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { RecipeDetailDisplayComponent } from './recipes/recipe-detail-display/re
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
-import { RecipeSearchBarComponent } from './recipes/recipe-search-bar/recipe-search-bar.component';
 import {Ng2SearchPipeModule} from 'ng2-search-filter';
 import {FormsModule} from '@angular/forms';
 
@@ -32,7 +31,6 @@ import {FormsModule} from '@angular/forms';
     RecipeListComponent,
     RecipesComponent,
     NavbarComponent,
-    RecipeSearchBarComponent
   ],
   imports: [
     BrowserModule,
